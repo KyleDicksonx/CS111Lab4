f7030fff7095bc8912db983c021f2c12f4620416
redma <redma@10.10.23.110>
2025-05-02 15:11:13 -0700

    decryptLevelTwo method finished.

--- CS111Lab4/src/cs111Lab2/Decryptor.java
+++ CS111Lab4/src/cs111Lab2/Decryptor.java
@@ -3,7 +3,13 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
 public class Decryptor {
+	
 	public String decryptLevelOne(String inputText) {
+		
+		
+		
+		
+		
 		
 	}
 	public String decryptLevelTwo(String inputText) {
@@ -11,8 +17,7 @@ public class Decryptor {
 		
 		/* using String's replace looks simpler than this
 		 * I made this before reading all of the lab
-		 * Idk if it even works, never tested this section before 
-		 * removing it
+		 * Idk if it even works, never tested this section before removing it
 	 	StringBuilder input = new StringBuilder(inputText);
 		for (int i = 0; i < input.length(); i++) {
 			
@@ -36,29 +41,35 @@ public class Decryptor {
 		modifiedInput.reverse();
 		
 		
-		String switchCase = modifiedInput.toString();
+		char[] switchCaseString = modifiedInput.toString().toCharArray();
 		for (int i = 0; i < modifiedInput.length(); i++) {
-			switchCase.toCharArray();
-			
-			
-			
+			if (Character.isLowerCase(switchCaseString[i])) {
+				Character.toUpperCase(switchCaseString[i]);
+			}
+			else if (Character.isUpperCase(switchCaseString[i])) {
+				Character.toLowerCase(switchCaseString[i]);
+			}
 			
 		}
 		
-		
+		return switchCaseString.toString();
 		
 		
 		
 		
 	}
+	
+	
 	public String readMessage(String fileName) throws IOException{
 		File inputFile = new File(fileName);
-		Scanner inputFileScanner = Scanner(inputFile);
+		Scanner inputFileScanner = new Scanner(inputFile);
 		String outputString = inputFileScanner.nextLine();
 		inputFileScanner.close();
 		return outputString;
 		
 	}
+	
+	
 	public static void main(String[] args) throws IOException{
 		
 		
