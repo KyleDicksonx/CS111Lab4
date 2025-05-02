65bacca29fff288eb05d33fd703e1ee870eb217c
redma <redma@10.10.23.110>
2025-05-02 15:57:08 -0700

    its working now, no comments yet

--- CS111Lab4/src/cs111Lab2/Decryptor.java
+++ CS111Lab4/src/cs111Lab2/Decryptor.java
@@ -4,15 +4,40 @@ import java.io.IOException;
 import java.util.Scanner;
 public class Decryptor {
 	
-	public String decryptLevelOne(String inputText) {
+	public static void main(String[] args) throws IOException{
 		
+		String inputText = readMessage("EncryptedMessage.txt");
 		
-		
-		
+		inputText = decryptLevelTwo(inputText);
+		//System.out.println("main lvl 2 done: " + inputText);
+		inputText = decryptLevelOne(inputText);
+		//System.out.println("main lvl 1 done: " + inputText);
+		System.out.println(inputText);
 		
 		
 	}
-	public String decryptLevelTwo(String inputText) {
+	
+	
+	
+	
+	public static String decryptLevelOne(String inputText) {
+		
+		StringBuilder codePointString = new StringBuilder(inputText);
+		StringBuilder newString = new StringBuilder();
+		char codePoint;
+		
+		
+		for (int i = 0; i < codePointString.length(); i++) {
+			codePoint = (char) (codePointString.codePointAt(i));
+			codePoint += 13;
+			newString.append(codePoint);
+		}
+		
+		//System.out.println("Lvl 1 end:  " + newString.toString());
+		return newString.toString();
+		
+	}
+	public static String decryptLevelTwo(String inputText) {
 		
 		
 		/* using String's replace looks simpler than this
@@ -30,29 +55,40 @@ public class Decryptor {
 		}
 		*/
 		
-		inputText.replace('!', 'a');
-		inputText.replace('{', 'x');
+		inputText = inputText.replace('!', 'a');
+		inputText = inputText.replace('{', 'x');
 		
 		
 		StringBuilder modifiedInput = new StringBuilder(inputText);
-		modifiedInput.delete(0, 1);
-		modifiedInput.delete(modifiedInput.length()-2, modifiedInput.length()-1);
-		
-		modifiedInput.reverse();
+		modifiedInput = modifiedInput.delete(0, 2);
+		//System.out.println("del section 1 end: " + modifiedInput);
+		modifiedInput = modifiedInput.delete(modifiedInput.length()-2, modifiedInput.length());
+		//System.out.println("del section 2 end: " + modifiedInput);
+		modifiedInput = modifiedInput.reverse();
 		
 		
 		char[] switchCaseString = modifiedInput.toString().toCharArray();
 		for (int i = 0; i < modifiedInput.length(); i++) {
 			if (Character.isLowerCase(switchCaseString[i])) {
-				Character.toUpperCase(switchCaseString[i]);
+				switchCaseString[i] = Character.toUpperCase(switchCaseString[i]);
+				//System.out.println("Switch a lower");
 			}
 			else if (Character.isUpperCase(switchCaseString[i])) {
-				Character.toLowerCase(switchCaseString[i]);
+				switchCaseString[i] = Character.toLowerCase(switchCaseString[i]);
+				//System.out.println("Switch an upper");
 			}
-			
 		}
 		
-		return switchCaseString.toString();
+		//System.out.println("lvl 2 end: " + switchCaseString.toString());
+		
+		//building a new string out of the char array because switchCaseString.toString() didnt work
+		//it was a headache finding that one
+		StringBuilder returnString = new StringBuilder();
+		for (int i = 0; i < switchCaseString.length; i++) {
+			returnString.append(switchCaseString[i]);
+			//System.out.println("level 2 return string build stages: " + returnString.toString());
+		}
+		return returnString.toString();
 		
 		
 		
@@ -60,7 +96,7 @@ public class Decryptor {
 	}
 	
 	
-	public String readMessage(String fileName) throws IOException{
+	public static String readMessage(String fileName) throws IOException{
 		File inputFile = new File(fileName);
 		Scanner inputFileScanner = new Scanner(inputFile);
 		String outputString = inputFileScanner.nextLine();
@@ -70,10 +106,5 @@ public class Decryptor {
 	}
 	
 	
-	public static void main(String[] args) throws IOException{
-		
-		
-		
-		
-	}
+	
 }
